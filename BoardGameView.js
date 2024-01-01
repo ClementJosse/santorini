7e23d898c3c632e4bd3a8faf50133f1ea2e48d19
JOSSE Clément <josse@et.esiea.fr>
2024-01-01 18:05:39 +0100

    définition par type de case, déplacement du pion en fonction de la hauteur de la case

--- BoardGameView.js
+++ BoardGameView.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { View, Image, Button, StyleSheet, Dimensions, TouchableOpacity } from 'react-native';
-import { getDatabase, ref, set, child, get, update, remove, onValue } from "firebase/database";
+import { update, ref } from 'firebase/database';
 import { db } from './firebaseconfig';
 
-const calcul = -((100) / (450)) * (Dimensions.get('window').width / 5);
+const calcul = -((100) / 450) * (Dimensions.get('window').width / 5);
 
 const images = {
   0: require('./assets/0.png'),
@@ -16,25 +16,25 @@ const images = {
 const BoutonTest = (codePartie, whoAmI) => {
   const newTurn = whoAmI === 'host' ? 'guest' : 'host';
   update(ref(db, 'games/' + codePartie), {
-    turn: newTurn
+    turn: newTurn,
   }).catch((error) => {
     alert(error);
   });
 };
 
-const UpdateBoard = (codePartie, rowIndex, colIndex,currentGameData) => {
+const UpdateBoard = (codePartie, rowIndex, colIndex, currentGameData) => {
   update(ref(db, `games/${codePartie}/board/${colIndex}`), {
-    [rowIndex]: (currentGameData.board[colIndex][rowIndex]+1)%5,
+    [rowIndex]: (currentGameData.board[colIndex][rowIndex] + 1) % 5,
   }).catch((error) => {
     alert(error);
   });
-}
+};
 
 const BoardGameView = ({ whoAmI, currentGameData, codePartie }) => {
   const numRows = 5;
   const numCols = 5;
 
-  const getImage = (colIndex,rowIndex) => {
+  const getImage = (colIndex, rowIndex) => {
     return images[currentGameData.board[colIndex][rowIndex]];
   };
 
@@ -45,15 +45,23 @@ const BoardGameView = ({ whoAmI, currentGameData, codePartie }) => {
           key={colIndex.toString()}
           onPress={() => {
             console.log(`Clicked at position [${rowIndex};${colIndex}]`);
-            if(currentGameData.turn === whoAmI){
-              UpdateBoard(codePartie,rowIndex,colIndex,currentGameData);
+            if (currentGameData.turn === whoAmI) {
+              UpdateBoard(codePartie, rowIndex, colIndex, currentGameData);
             }
           }}
         >
-          <Image
-            source={getImage(colIndex,rowIndex)}
-            style={[styles.image, colIndex !== 0 && { marginLeft: calcul }]}
-          />
+          <View style={styles.cell}>
+            <Image
+              source={getImage(colIndex, rowIndex)}
+              style={[styles.image, colIndex !== 0 && { marginLeft: calcul }]}
+            />
+            {rowIndex === 3 && (
+              <Image
+                source={require('./assets/1pion_o.png')}
+                style={[styles.overlayImage, colIndex !== 0 && { marginLeft: calcul },{ position: 'absolute', top: 0, left: 0 }]}
+              />
+            )}
+          </View>
         </TouchableOpacity>
       ))}
     </View>
@@ -63,17 +71,17 @@ const BoardGameView = ({ whoAmI, currentGameData, codePartie }) => {
     <View style={styles.container}>
       {Array.from({ length: numRows }).map((_, rowIndex) => renderRow(rowIndex))}
       <View style={styles.row}>
-        <Image source={require('./assets/pion_o.png')} style={styles.image} />
-        <Image source={require('./assets/pion_o.png')} style={styles.image} />
-        <Image source={require('./assets/pion_v.png')} style={styles.image} />
-        <Image source={require('./assets/pion_v.png')} style={styles.image} />
+        <Image source={require('./assets/0pion_o.png')} style={styles.image} />
+        <Image source={require('./assets/0pion_o.png')} style={styles.image} />
+        <Image source={require('./assets/0pion_v.png')} style={styles.image} />
+        <Image source={require('./assets/0pion_v.png')} style={styles.image} />
       </View>
       {currentGameData.turn === whoAmI && (
         <Button
           title="TOUR"
           onPress={() => {
             console.log(whoAmI);
-            console.log(currentGameData.board)
+            console.log(currentGameData.board);
             BoutonTest(codePartie, whoAmI);
           }}
         />
@@ -90,12 +98,19 @@ const styles = StyleSheet.create({
   },
   row: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
     marginBottom: calcul,
   },
   image: {
     width: Dimensions.get('window').width / 5,
     height: Dimensions.get('window').width / 5,
+  },
+  overlayImage: {
+    width: Dimensions.get('window').width / 5,
+    height: Dimensions.get('window').width / 5,
+    resizeMode: 'cover',
+  },
+  cell: {
+    position: 'relative',
   },
 });
 
